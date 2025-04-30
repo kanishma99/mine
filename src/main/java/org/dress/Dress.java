61aa23307f6666fdda698fcb3dc41bbf82e7cb98
kanishma99 <kanishma.kani99@gmail.com>
2025-04-30 14:37:52 +0530

    add codes

---
+++ src/main/java/org/dress/Dress.java
@@ -0,0 +1,17 @@
+package org.dress;
+
+public class Dress {
+	private void black() {
+		System.out.println("1");
+
+	}
+	private void white() {
+		System.out.println("2");
+
+	}
+	private void green() {
+		System.out.println("3");
+
+	}
+
+}
